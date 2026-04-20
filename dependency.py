8f5e220820da290f7c120fe1fba5e359a2915d72
Qryptic-0 <onize.ummiqulsum@gmail.com>
2026-04-20 15:32:33 +0100

    Add token verification dependency for FastAPI

---
+++ dependency.py
@@ -0,0 +1,7 @@
+from fastapi import Header, HTTPException
+
+# Simple Auth Dependency
+async def verify_token(x_token: str = Header(...)):
+    if x_token != "super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+
